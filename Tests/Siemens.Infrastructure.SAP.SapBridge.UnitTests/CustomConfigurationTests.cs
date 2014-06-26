f0f7b0f9c1099a9f77fab85447a9a1f4705403fe
Antonio Elena <antonioelena@gmail.com>
2014-06-26 13:00:12 +0200

    ShouldContainCorrectApplicationNode test added and passed

--- Tests/Siemens.Infrastructure.SAP.SapBridge.UnitTests/CustomConfigurationTests.cs
+++ Tests/Siemens.Infrastructure.SAP.SapBridge.UnitTests/CustomConfigurationTests.cs
@@ -3,6 +3,7 @@ using Siemens.Infrastructure.SAP.SapBridge.Configuration.Tools;
 using Xunit;
 using FluentAssertions;
 using System.Xml.Linq;
+using System.Linq;
 
 namespace Siemens.Infrastructure.SAP.SapBridge.UnitTests
 {
@@ -10,16 +11,34 @@ namespace Siemens.Infrastructure.SAP.SapBridge.UnitTests
     {
 
         [Fact]
-        public void ShouldGetRootElementRight ()
+        public void ShouldContainCorrectRootElement ()
         {
 
             var root = new SapConfigurationSection ();
-            root.Entries.Add ( new SapConfigurationEntry () { Company = "1234", Environment = "Q" } );
+            root.Entries.Add ( new ApplicationEntry ( "MDM" ) );
 
             var _xml = XmlSerializerSectionHandler.SerializeObject ( root );
 
             var xDoc = XDocument.Parse ( _xml );
             xDoc.Root.Name.Should ().Equals ( "SapConfigurationSection" );
+
+        }
+
+
+        [Fact]
+        public void ShouldContainCorrectApplicationNode ()
+        {
+
+            var root = new SapConfigurationSection ();
+            root.Entries.Add ( new ApplicationEntry ( "MDM" ) );
+
+            var _xml = XmlSerializerSectionHandler.SerializeObject ( root );
+
+            var xDoc = XDocument.Parse ( _xml );
+            var appNode = from applications in xDoc.Descendants(XName.Get("Application"))
+                          select applications;
+
+            appNode.First ().FirstAttribute.Value.Should ().Equals ( "MDM" );
 
 
         }
