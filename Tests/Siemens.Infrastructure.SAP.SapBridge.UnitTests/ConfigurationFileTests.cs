0e3ce064326d68d631cbd09abf081a4f8ac127c2
Antonio Elena <antonioelena@gmail.com>
2014-06-26 18:51:13 +0200

    App.Config and ConfigurationFIleTests added

---
+++ Tests/Siemens.Infrastructure.SAP.SapBridge.UnitTests/ConfigurationFileTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+using Siemens.Infrastructure.SAP.SapBridge.Configuration;
+
+namespace Siemens.Infrastructure.SAP.SapBridge.UnitTests
+{
+    public class ConfigurationFileTests
+    {
+        SapConfigurationSection _config = null;
+        public ConfigurationFileTests ()
+        {
+            _config = ( SapConfigurationSection ) ConfigurationManager.GetSection ( "SapConfigurationSection" );
+        }
+
+        [Fact]
+        public void ShouldGetConfigurationSection ()
+        {
+            _config.Should ().NotBeNull ();
+        }
+
+
+        [Fact]
+        public void ShouldGetTwoApplications ()
+        {
+            _config.Entries.Should ().HaveCount ( 2 );
+        }
+
+        [Fact]
+        public void ShouldGetTwoCompanyCodesConfiguredPerApplication ()
+        {
+            _config.Entries.First ().SapConfigurationEntries.Should ().HaveCount ( 2 );
+        }
+
+        [Fact]
+        public void ShouldHaveCorrectCompanyCodesForEachConfigurationEntry ()
+        {
+            _config.Entries.First ().SapConfigurationEntries.FindAll ( x => x.Company == "1234" ).Count ().Should ().Be ( 1 );
+            _config.Entries.First ().SapConfigurationEntries.FindAll ( x => x.Company == "5290" ).Count ().Should ().Be ( 1 );
+            _config.Entries.First ().SapConfigurationEntries.FindAll ( x => x.Environment == "Q" ).Count ().Should ().Be ( 2 );
+        }
+
+    }
+}
