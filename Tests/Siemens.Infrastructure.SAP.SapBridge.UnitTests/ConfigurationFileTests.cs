f1dd225df2c3749445e44984817ab67101301705
Antonio Elena <antonioelena@gmail.com>
2014-06-26 23:16:53 +0200

    small increments in testing

--- Tests/Siemens.Infrastructure.SAP.SapBridge.UnitTests/ConfigurationFileTests.cs
+++ Tests/Siemens.Infrastructure.SAP.SapBridge.UnitTests/ConfigurationFileTests.cs
@@ -18,24 +18,33 @@ namespace Siemens.Infrastructure.SAP.SapBridge.UnitTests
             _config = ( SapConfigurationSection ) ConfigurationManager.GetSection ( "SapConfigurationSection" );
         }
 
+        // ---------------------------------------------------------------------------------------------
+
         [Fact]
         public void ShouldGetConfigurationSection ()
         {
             _config.Should ().NotBeNull ();
         }
 
+        // ---------------------------------------------------------------------------------------------
 
         [Fact]
         public void ShouldGetTwoApplications ()
         {
             _config.Entries.Should ().HaveCount ( 2 );
+            _config.Entries.First ().Name.Should ().BeEquivalentTo ( "MDM" );
+            _config.Entries.Last ().Name.Should ().BeEquivalentTo ( "MDMC" );
         }
+
+        // ---------------------------------------------------------------------------------------------
 
         [Fact]
         public void ShouldGetTwoCompanyCodesConfiguredPerApplication ()
         {
             _config.Entries.First ().SapConfigurationEntries.Should ().HaveCount ( 2 );
         }
+
+        // ---------------------------------------------------------------------------------------------
 
         [Fact]
         public void ShouldHaveCorrectCompanyCodesForEachConfigurationEntry ()
@@ -44,6 +53,28 @@ namespace Siemens.Infrastructure.SAP.SapBridge.UnitTests
             _config.Entries.First ().SapConfigurationEntries.FindAll ( x => x.Company == "5290" ).Count ().Should ().Be ( 1 );
             _config.Entries.First ().SapConfigurationEntries.FindAll ( x => x.Environment == "Q" ).Count ().Should ().Be ( 2 );
         }
+
+        // ---------------------------------------------------------------------------------------------
+
+        [Fact]
+        public void ShouldHaveTwoBapiConfiguration ()
+        {
+            _config.Entries.First ().SapConfigurationEntries.First ().BapiConfigurations.BapiConfigurations.Should ().HaveCount ( 2 );
+            _config.Entries.First ().SapConfigurationEntries.Last ().BapiConfigurations.BapiConfigurations.Should ().HaveCount ( 2 );
+        }
+
+        // ---------------------------------------------------------------------------------------------
+
+        [Fact]
+        public void ShouldHaveCorrectBapiConfiguration2 ()
+        {
+            _config.Entries.First ().SapConfigurationEntries.First ().BapiConfigurations.BapiConfigurations.First ().BapiName.Should ().BeEquivalentTo ( "/SIE/SWE_MM_GRTO1" );
+            _config.Entries.First ().SapConfigurationEntries.First ().BapiConfigurations.BapiConfigurations.Last ().BapiName.Should ().BeEquivalentTo ( "/SIE/SWE_MM_GRTO2" );
+            _config.Entries.Last ().SapConfigurationEntries.Last ().BapiConfigurations.BapiConfigurations.First ().BapiName.Should ().BeEquivalentTo ( "/SIE/SWE_MM_XAXA1" );
+            _config.Entries.Last ().SapConfigurationEntries.Last ().BapiConfigurations.BapiConfigurations.Last ().BapiName.Should ().BeEquivalentTo ( "/SIE/SWE_MM_XAXA2" );
+        }
+
+        // ---------------------------------------------------------------------------------------------
 
     }
 }
