8e12b534705877f8c560abbcdefbb2fcb5069cf6
Antonio Elena <antonioelena@gmail.com>
2014-06-26 12:18:32 +0200

    added test project

---
+++ Tests/Siemens.Infrastructure.SAP.SapBridge.UnitTests/CustomConfigurationTests.cs
@@ -0,0 +1,30 @@
+﻿using Siemens.Infrastructure.SAP.SapBridge.Configuration;
+using Siemens.Infrastructure.SAP.SapBridge.Configuration.Tools;
+using Xunit;
+using FluentAssertions;
+using System.Xml.Linq;
+
+namespace Siemens.Infrastructure.SAP.SapBridge.UnitTests
+{
+    public class CustomConfigurationTests
+    {
+
+        [Fact]
+        public void ShouldGetRootElementRight ()
+        {
+
+            var root = new SapConfigurationSection ();
+            root.Entries.Add ( new SapConfigurationEntry () { Company = "1234", Environment = "Q" } );
+
+            var _xml = XmlSerializerSectionHandler.SerializeObject ( root );
+
+            var xDoc = XDocument.Parse ( _xml );
+            xDoc.Root.Name.Should ().Equals ( "SapConfigurationSection" );
+
+
+        }
+
+
+    }
+
+}
