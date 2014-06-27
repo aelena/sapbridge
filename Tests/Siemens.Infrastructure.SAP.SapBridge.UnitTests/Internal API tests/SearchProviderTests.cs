d7adbd22e6c111fc585f873488030eadb828873d
Antonio Elena <antonioelena@gmail.com>
2014-06-27 17:39:12 +0200

    CodingTests class added

---
+++ Tests/Siemens.Infrastructure.SAP.SapBridge.UnitTests/Internal API tests/SearchProviderTests.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Siemens.Infrastructure.SAP.SapBridge.Configuration;
+using Xunit;
+using SAP.Middleware.Connector;
+
+namespace Siemens.Infrastructure.SAP.SapBridge.UnitTests.Internal_API_tests
+{
+    public class SearchProviderTests
+    {
+
+        SapConfigurationSection _config = null;
+        public SearchProviderTests ()
+        {
+            _config = ( SapConfigurationSection ) ConfigurationManager.GetSection ( "SapConfigurationSection" );
+        }
+
+        // ---------------------------------------------------------------------------------------------
+
+        [Fact]
+        public void ShouldGetCorrectConfigurationData ()
+        {
+
+        }
+
+        // ---------------------------------------------------------------------------------------------
+
+        [Fact]
+        public void ShouldMapConnectionDataToRFC ()
+        {
+            var sp = new SearchProvider ();
+            var results = sp.GetConnectionParameters ( "MDM", "Q", "SEARCH", "5290" );
+            // check some data to make sure the configuration read is the right one
+            results [ "ASHOST" ].Should ().BeEquivalentTo ( "unknown" );
+            results [ "USER" ].Should ().BeEquivalentTo ( "user1" );
+            results [ "CLIENT" ].Should ().BeEquivalentTo ( "74" );
+
+        }
+
+        // ---------------------------------------------------------------------------------------------
+
+        [Fact]
+        public void ShouldMapConnectionDataToRFCUsingExistingConfiguration ()
+        {
+
+            var sp = new SearchProvider ();
+            var results = sp.GetConnectionParameters ( "MDM", "Q", "SEARCH", "5290", 
+                ( SapConfigurationSection ) ConfigurationManager.GetSection ( "SapConfigurationSection" ) );
+            // check some data to make sure the configuration read is the right one
+            results [ "ASHOST" ].Should ().BeEquivalentTo ( "unknown" );
+            results [ "USER" ].Should ().BeEquivalentTo ( "user1" );
+            results [ "CLIENT" ].Should ().BeEquivalentTo ( "74" );
+
+        }
+
+        // ---------------------------------------------------------------------------------------------
+
+
+        // ---------------------------------------------------------------------------------------------
+
+
+        // ---------------------------------------------------------------------------------------------
+
+
+        // ---------------------------------------------------------------------------------------------
+
+
+        // ---------------------------------------------------------------------------------------------
+
+    }
+}
---
+++ Tests/Siemens.Infrastructure.SAP.SapBridge.UnitTests/_code tests/CodingTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using FluentAssertions;
+
+namespace Siemens.Infrastructure.SAP.SapBridge.UnitTests._code_tests
+{
+    public class CodingTests
+    {
+
+        [Fact]
+        public void CodeTest_CheckingDynamicObject ()
+        {
+
+            dynamic d = new
+            {
+                MyFirstField = "First",
+                MySecondField = "Second",
+                MyThirdField = 3
+            };
+            var props = ( ( object ) d ).GetType ().GetProperties ();
+            props.Should ().HaveCount ( 3 );
+            var hash = from p in props
+                       select new
+                       {
+                           Key = p.Name,
+                           Value = p.GetValue ( d )
+                       };
+
+            hash.Should ().HaveCount ( 3 );
+            hash.First ().Value.Equals ( "First" );
+            hash.ElementAt ( 2 ).Value.Equals ( "Second" );
+            hash.Last ().Value.Equals ( 3 );
+
+        }
+
+    }
+}
