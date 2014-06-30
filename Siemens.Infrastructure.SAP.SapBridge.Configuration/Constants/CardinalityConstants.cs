1b0e56b521b3d2e7c369b37c67ea9bd146095cc5
Antonio Elena <antonioelena@gmail.com>
2014-06-30 13:19:02 +0200

    CardinalityConstants class added test included

---
+++ Siemens.Infrastructure.SAP.SapBridge.Configuration/Constants/CardinalityConstants.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Siemens.Infrastructure.SAP.SapBridge.Configuration.Constants
+{
+
+    /// <summary>
+    /// This class holds a constants indicating different
+    /// values for instance cardinalities.
+    /// There can be several cardinalities expressed by
+    /// the values
+    ///
+    /// 0-1 - zero or one element max, meaning an optional mapping
+    /// 1   - mandatory and exactly only one instance
+    /// 0-n - zero or 1 or several instances
+    /// 1-n - at least one instance of the mapping to be sent to SAP
+    /// 
+    /// </summary>
+    public sealed class CardinalityConstants
+    {
+        /// <summary>
+        /// 0-1 - zero or one element max, meaning an optional element
+        /// </summary>
+        public const string ZeroOrOne = "0-1";
+        /// <summary>
+        /// 1   - mandatory and exactly only one instance
+        /// </summary>
+        public const string ExactlyOne = "1";
+        /// <summary>
+        /// 0-n - zero or 1 or several instances
+        /// </summary>
+        public const string ZeroOrN = "0-n";
+        /// <summary>
+        /// 1-n - at least one instance of the mapping to be sent to SAP
+        /// </summary>
+        public const string OneOrN = "1-n";
+    }
+}
---
+++ Tests/Siemens.Infrastructure.SAP.SapBridge.UnitTests/Internal API tests/CardinalityTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using FluentAssertions;
+using Siemens.Infrastructure.SAP.SapBridge.Configuration.Constants;
+
+namespace Siemens.Infrastructure.SAP.SapBridge.UnitTests.Internal_API_tests
+{
+    public class CardinalityTests
+    {
+
+        [Fact]
+        public void ShouldValidateCorrectCardinality()
+        {
+            CardinalityConstants.ExactlyOne.Should ().BeEquivalentTo ( "1" );
+            CardinalityConstants.OneOrN.Should ().BeEquivalentTo ( "1-n" );
+            CardinalityConstants.ZeroOrN.Should ().BeEquivalentTo ( "0-n" );
+            CardinalityConstants.ZeroOrOne.Should ().BeEquivalentTo ( "0-1" );
+        }
+
+    }
+}
