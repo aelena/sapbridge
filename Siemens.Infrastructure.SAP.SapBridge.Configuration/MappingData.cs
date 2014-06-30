1ae63d6465836e1794ec175368e377b7b99b4e70
Antonio Elena <antonioelena@gmail.com>
2014-06-30 09:25:13 +0200

    added cardinality and typeName attributes

--- Siemens.Infrastructure.SAP.SapBridge.Configuration/MappingData.cs
+++ Siemens.Infrastructure.SAP.SapBridge.Configuration/MappingData.cs
@@ -9,8 +9,35 @@ namespace Siemens.Infrastructure.SAP.SapBridge.Configuration
     public class MappingData
     {
 
+        /// <summary>
+        /// Gets or sets a value that represents the technical
+        /// name of the table in SAP.
+        /// </summary>
         [XmlAttribute ( AttributeName = "tableName" )]
-        public string TableName { get; set; } 
+        public string TableName { get; set; }
+
+        /// <summary>
+        /// Gets or sets a a value that indicates the managed
+        /// type that will contain the values that will be passed
+        /// to the SAP fields defined in the mappings.
+        /// </summary>
+        [XmlAttribute ( AttributeName = "typeName" )]
+        public string TypeName { get; set; }
+
+        /// <summary>
+        /// Gets or sets a a value that indicates the cardinality
+        /// of the mapping. 
+        /// There can be several cardinalities expressed by
+        /// the values
+        ///
+        /// 0-1 - zero or one element max, meaning an optional mapping
+        /// 1   - mandatory and exactly only one instance
+        /// 0-n - zero or 1 or several instances
+        /// 1-n - at least one instance of the mapping to be sent to SAP
+        /// 
+        /// </summary>
+        [XmlAttribute ( AttributeName = "cardinality" )]
+        public string Cardinality { get; set; } 
 
         [XmlElement ( ElementName = "Mapping" )]
         public List<Mapping> Mappings = new List<Mapping> ();
