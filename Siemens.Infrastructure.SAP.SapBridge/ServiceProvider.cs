bad214037333adaf5652796624ec18d84f8f03b6
Antonio Elena <antonioelena@gmail.com>
2014-06-30 14:32:37 +0200

    searchDataCriteria

--- Siemens.Infrastructure.SAP.SapBridge/ServiceProvider.cs
+++ Siemens.Infrastructure.SAP.SapBridge/ServiceProvider.cs
@@ -16,18 +16,25 @@ namespace Siemens.Infrastructure.SAP.SapBridge
         /// <typeparam name="R"></typeparam>
         /// <param name="applicationCode">THe short letter code that identifies an application, such as 
         /// MDM, MDMC, GRT, TTT, etc.</param>
+        /// <param name="environment"></param>
         /// <param name="operation">A literal that indicates the operation to perform</param>
         /// <param name="companyCode">An optional company code parameter to find specific operations
         /// defined for specific company codes in the .config file.</param>
+        /// <param name="searchDataCriteria">Represents any object (a DTO basically) that contains
+        /// at least the minimum fields needed to perform the search against SAP. Since the number
+        /// of fields to be used in search will vary from application to application - or even to
+        /// perform different searches within one single application - we need to keep this 
+        /// flexible.</param>
         /// <returns></returns>
-        public IQueryable<R> PerformSearch<T,R> ( string applicationCode, 
+        public IQueryable<R> PerformSearch<T,R> ( object searchDataCriteria,
+                                                  string applicationCode, 
                                                   string environment,
                                                   string operation, 
                                                   string companyCode = "*" )
         {
 
             var sp = new SearchProvider ();
-            return sp.Search<T,R> ( applicationCode, environment, operation, companyCode );
+            return sp.Search<T, R> ( searchDataCriteria, applicationCode, environment, operation, companyCode );
 
         }
 
