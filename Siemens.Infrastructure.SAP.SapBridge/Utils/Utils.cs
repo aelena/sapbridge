0b57f3c8686d4f9650ac18e938d3558493fcf762
Antonio Elena <antonioelena@gmail.com>
2014-07-01 11:51:51 +0200

    Utils class added - with corresponding tests

---
+++ Siemens.Infrastructure.SAP.SapBridge/Utils/Utils.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Siemens.Infrastructure.SAP.SapBridge
+{
+    public sealed class Utils
+    {
+
+        /// <summary>
+        /// Creates a simple instance of a DataTable
+        /// where all the fields are string by default.
+        /// Only a list of names for the columns has to be passed.
+        /// </summary>
+        /// <param name="columnNames"></param>
+        /// <returns></returns>
+        public DataTable CreateSimpleTable ( IEnumerable<string> columnNames )
+        {
+            if ( columnNames == null )
+                return null;
+
+            var dt = new DataTable();
+            foreach ( var colName in columnNames )
+                dt.Columns.Add ( new DataColumn ( colName, typeof ( String ) ) );
+            return dt;
+        }
+
+        // ---------------------------------------------------------------------------------------------
+
+
+    }
+}
---
+++ Tests/Siemens.Infrastructure.SAP.SapBridge.UnitTests/UtilsTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using FluentAssertions;
+
+namespace Siemens.Infrastructure.SAP.SapBridge.UnitTests
+{
+    public class UtilsTests
+    {
+        [Fact]
+        public void SimpleDataTableCreationShouldReturnNullOnNullList()
+        {
+            var u = new Utils ();
+            u.CreateSimpleTable ( null ).Should ().BeNull ();
+        }
+
+        // ---------------------------------------------------------------------------------------------
+
+        [Fact]
+        public void SimpleDataTableCreationShouldReturnEmptyTableOnEmptyList ()
+        {
+            var u = new Utils ();
+            u.CreateSimpleTable ( new List<string>() ).Columns.Should ().HaveCount ( 0 );
+        }
+
+        // ---------------------------------------------------------------------------------------------
+
+        [Fact]
+        public void SimpleDataTableCreationShouldCreateCorrectly ()
+        {
+            var u = new Utils ();
+            u.CreateSimpleTable ( new List<String> { "COL_A", "COL_B", "COL_C" } ).Columns.Should ().HaveCount ( 3 );
+        }
+
+        // ---------------------------------------------------------------------------------------------
+
+    }
+}
