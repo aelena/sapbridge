dc0a44c47f791e796ee661ef760a5c3af41d62bd
Antonio Elena <antonioelena@gmail.com>
2014-07-08 20:09:46 +0200

    Comments in DeltaItem
    adding untracked files missing from previous commit

--- Siemens.W4E.SAP.DeltaService/DeltaItem.cs
+++ Siemens.W4E.SAP.DeltaService/DeltaItem.cs
@@ -5,33 +5,69 @@ using System.Text;
 
 namespace Siemens.W4E.SAP.DeltaService
 {
-    public sealed class DeltaItem
+    /// <summary>
+    /// Represents the simplest information snippet about 
+    /// what changed between two instances of a type.
+    /// It has only three fields
+    /// 
+    /// - FieldName : name of the instance field that is different from one instance to the other
+    /// - FieldValueInFirstInstance : value of the field in the first instance
+    /// - FieldValueInSecondInstance : value of the field in the second instance
+    /// </summary>
+    public class DeltaItem
     {
 
         private readonly string _fieldName;
         private readonly object _firstInstanceValue;
         private readonly object _secondInstanceValue;
 
+        /// <summary>
+        /// Gets the name of the instance field that is different from one instance to the other.
+        /// </summary>
         public string FieldName
         {
             get { return this._fieldName; }
         }
 
+        /// <summary>
+        /// Gets the value of the field in the first instance
+        /// </summary>
         public object FieldValueInFirstInstance
         {
             get { return this._firstInstanceValue; }
         }
 
+        /// <summary>
+        /// Gets the  value of the field in the second instance
+        /// </summary>
         public object FieldValueInSecondInstance
         {
             get { return this._secondInstanceValue; }
         }
 
+        /// <summary>
+        /// Parameterized constructor.
+        /// </summary>
+        /// <param name="fieldName"></param>
+        /// <param name="firstInstanceValue"></param>
+        /// <param name="secondInstanceValue"></param>
         public DeltaItem (string fieldName, object firstInstanceValue, object secondInstanceValue)
         {
             this._fieldName = fieldName;
             this._firstInstanceValue = firstInstanceValue;
             this._secondInstanceValue = secondInstanceValue;
+        }
+
+
+        /// <summary>
+        /// Overriding ToString() to return a more readable representation
+        /// of the object.
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString ()
+        {
+            return string.Format ( "Field '{0}' : in origin {0}, in second instance {1}", this.FieldName,
+                this.FieldValueInFirstInstance.ToString(), this.FieldValueInSecondInstance.ToString() );
         }
 
     }
---
+++ Siemens.W4E.SAP.DeltaService/DeltaProvider.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Ploeh.Albedo;
+
+namespace Siemens.W4E.SAP.DeltaService
+{
+    public class DeltaProvider
+    {
+
+        public IEnumerable<DeltaItem> FindDelta<T> ( T original,
+                                     T newer,
+                                     bool deltaOnlyForValueOrStringFields = true,
+                                     bool doRecursiveInspect = false,
+                                     BindingFlags bindingFlags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic )
+        {
+            // would not make much sense in either case...
+            if ( original == null || newer == null )
+                return null;
+
+            var _origFP = original.GetType ().GetPropertiesAndFields ( bindingFlags );
+            var _newFP = newer.GetType ().GetPropertiesAndFields ( bindingFlags );
+
+            var actual_1 = _origFP.Accept ( new ValueAndNameCollectingVisitor ( original ) ).Value.Cast<Tuple<string, object>> ().ToArray ();
+            var actual_2 = _newFP.Accept ( new ValueAndNameCollectingVisitor ( newer ) ).Value.Cast<Tuple<string, object>> ().ToArray ();
+
+            if ( deltaOnlyForValueOrStringFields && !doRecursiveInspect )
+            {
+                // this only works for strings or primitive types but not for recursive type inspection
+                // so we still have got some way to go here... 
+                var delta = actual_1.Zip ( actual_2, ( a1, a2 ) => ( a1.Item2.ToString() != a2.Item2.ToString() ) ? new DeltaItem ( a1.Item1, a1.Item2, a2.Item2 ) : null );
+                return delta.Where ( x => x != null );
+            }
+
+            return null;
+        }
+
+
+    }
+
+
+}
