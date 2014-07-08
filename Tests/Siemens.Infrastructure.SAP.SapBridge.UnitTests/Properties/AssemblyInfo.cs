daf5fdc1e557b5b70f03ba71e6c1443121ec81a6
Antonio Elena <antonioelena@gmail.com>
2014-07-08 17:07:20 +0200

    made internals visible to the other test project in order to reuse dummy types

--- Tests/Siemens.Infrastructure.SAP.SapBridge.UnitTests/Properties/AssemblyInfo.cs
+++ Tests/Siemens.Infrastructure.SAP.SapBridge.UnitTests/Properties/AssemblyInfo.cs
@@ -5,22 +5,22 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Siemens.Infrastructure.SAP.SapBridge.UnitTests")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Siemens.Infrastructure.SAP.SapBridge.UnitTests")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyTitle ( "Siemens.Infrastructure.SAP.SapBridge.UnitTests" )]
+[assembly: AssemblyDescription ( "" )]
+[assembly: AssemblyConfiguration ( "" )]
+[assembly: AssemblyCompany ( "" )]
+[assembly: AssemblyProduct ( "Siemens.Infrastructure.SAP.SapBridge.UnitTests" )]
+[assembly: AssemblyCopyright ( "Copyright ©  2014" )]
+[assembly: AssemblyTrademark ( "" )]
+[assembly: AssemblyCulture ( "" )]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible ( false )]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("09181b5a-cd49-46ce-a0ea-f2df5966c7fa")]
+[assembly: Guid ( "09181b5a-cd49-46ce-a0ea-f2df5966c7fa" )]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion ( "1.0.0.0" )]
+[assembly: AssemblyFileVersion ( "1.0.0.0" )]
+[assembly: InternalsVisibleTo ( "Siemens.W4E.SAP.DeltaService.UnitTests" )]
