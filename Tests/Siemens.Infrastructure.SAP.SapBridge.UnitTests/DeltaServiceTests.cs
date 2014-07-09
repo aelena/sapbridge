7f97551e3fcf983d44745416a4d5a7c06ea71c59
Antonio Elena <antonioelena@gmail.com>
2014-07-09 15:28:37 +0200

    moved DeltaServiceTests to this project

---
+++ Tests/Siemens.Infrastructure.SAP.SapBridge.UnitTests/DeltaServiceTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Siemens.Infrastructure.SAP.SapBridge.UnitTests.Dummies;
+using FluentAssertions;
+
+namespace Siemens.W4E.SAP.DeltaService.UnitTests
+{
+    public class DeltaServiceTests
+    {
+        [Fact]
+        public void ShouldFindSimplestDelta ()
+        {
+            // create two simples objects in the arrange phase
+            var fooOriginal = new SimpleFoo ();
+            var fooNew = new SimpleFoo { Message = "I am a newer instance", Age = 2828 };
+            // instantiate the service that returns the delta
+            var deltaProvider = new DeltaProvider ();
+            var delta = deltaProvider.FindDelta<SimpleFoo> ( fooOriginal, fooNew );
+            delta.Should ().HaveCount ( 2 );
+        }
+
+        [Fact]
+        public void ShouldNotCrashOnNullField ()
+        {
+            // create two simples objects in the arrange phase
+            var fooOriginal = new SimpleFoo ();
+            var fooNew = new SimpleFoo { Message = null, Age = 2828 };
+            // instantiate the service that returns the delta
+            var deltaProvider = new DeltaProvider ();
+            var delta = deltaProvider.FindDelta<SimpleFoo> ( fooOriginal, fooNew );
+            delta.Should ().HaveCount ( 2 );
+        }
+
+        [Fact]
+        public void ShouldNotCrashOnNullInstance_1 ()
+        {
+            var fooOriginal = new SimpleFoo ();
+            var deltaProvider = new DeltaProvider ();
+            var delta = deltaProvider.FindDelta<SimpleFoo> ( fooOriginal, null );
+            delta.Should ().BeNull ();
+        }
+
+        [Fact]
+        public void ShouldNotCrashOnNullInstance_2 ()
+        {
+            var fooOriginal = new SimpleFoo ();
+            var deltaProvider = new DeltaProvider ();
+            var delta = deltaProvider.FindDelta<SimpleFoo> ( null, fooOriginal );
+            delta.Should ().BeNull ();
+        }
+
+
+
+
+    }
+}
